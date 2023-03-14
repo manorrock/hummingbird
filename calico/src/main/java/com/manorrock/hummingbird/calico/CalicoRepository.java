52e9a85f13ea35d7643594f13a7555af337fac53
Manfred Riem <mriem@manorrock.com>
2023-03-14 16:57:13 -0500

    Fixes issue #36 - Add Manorrock Calico implementation (#41)

---
+++ calico/src/main/java/com/manorrock/hummingbird/calico/CalicoRepository.java
@@ -0,0 +1,57 @@
+/*
+ *  Copyright (c) 2002-2023, Manorrock.com. All Rights Reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *
+ *      1. Redistributions of source code must retain the above copyright
+ *         notice, this list of conditions and the following disclaimer.
+ *
+ *      2. Redistributions in binary form must reproduce the above copyright
+ *         notice, this list of conditions and the following disclaimer in the
+ *         documentation and/or other materials provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ *  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ *  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ *  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ *  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ *  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ *  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.manorrock.hummingbird.calico;
+
+import java.net.URI;
+import com.manorrock.hummingbird.api.Repository;
+import com.manorrock.hummingbird.api.RepositoryDirectory;
+
+/**
+ * The Manorrock Calico Repository implementation.
+ *
+ * @author Manfred Riem (mriem@manorrock.com)
+ */
+public class CalicoRepository implements Repository {
+
+    /**
+     * Stores the base URI.
+     */
+    private final URI baseUri;
+    
+    /**
+     * Constructor.
+     * 
+     * @param baseUri the base URI.
+     */
+    public CalicoRepository(URI baseUri) {
+        this.baseUri = baseUri;
+    }
+    
+    @Override
+    public RepositoryDirectory getRootDirectory() {
+        return new CalicoRepositoryDirectory(this);
+    }
+}
