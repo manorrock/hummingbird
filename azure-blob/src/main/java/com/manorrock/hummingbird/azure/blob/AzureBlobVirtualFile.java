90de9277e98e5ebad89348d73e55f6282a2bdc6b
Manfred Riem <mriem@manorrock.com>
2023-05-17 06:37:41 -0500

    Fixes issue #57 - Add Azure Blob Storage implementation (#67)

---
+++ azure-blob/src/main/java/com/manorrock/hummingbird/azure/blob/AzureBlobVirtualFile.java
@@ -0,0 +1,84 @@
+/*
+ *  Copyright (c) 2002-2023, Manorrock.com. All Rights Reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *
+ *      1. Redistributions of source code must retain the above copyright
+ *         notice, this list of conditions and the following disclaimer.
+ *
+ *      2. Redistributions in binary form must reproduce the above copyright
+ *         notice, this list of conditions and the following disclaimer in the
+ *         documentation and/or other materials provided with the distribution.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ *  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ *  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ *  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ *  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ *  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ *  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.manorrock.hummingbird.azure.blob;
+
+import java.io.InputStream;
+import com.manorrock.hummingbird.api.VirtualFile;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * The Azure Blob Storage VirtualFile implementation.
+ * 
+ * @author Manfred Riem (mriem@manorrock.com)
+ */
+public class AzureBlobVirtualFile implements VirtualFile {
+
+    /**
+     * Stores the path.
+     */
+    private final String path;
+
+    /**
+     * Stores the file system.
+     */
+    private final AzureBlobVirtualFileSystem fileSystem;
+    
+    /**
+     * Constructor.
+     * 
+     * @param fileSystem the file system.
+     * @param path the path.
+     */
+    public AzureBlobVirtualFile(AzureBlobVirtualFileSystem fileSystem, String path) {
+        this.fileSystem = fileSystem;
+        this.path = path;
+    }
+    
+    /**
+     * As an input stream.
+     * 
+     * @return an input stream, or null if not found or when an I/O error occurred.
+     */
+    @Override
+    public InputStream asInputStream() {
+        try {
+            return fileSystem.getInputStream(path);
+        } catch(IOException ioe) {
+            return null;
+        }
+    }
+
+    @Override
+    public VirtualFile getFile(String path) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public List<VirtualFile> getFiles() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
